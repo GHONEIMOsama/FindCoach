6ee7581ecad4fcdb1d8d5486421ae79586b6992e
Osama <ossamaghoneim4@gmail.com>
2024-01-04 15:56:49 +0100

    FEAT: switch between userLogin and CreateAccount on auth Page

--- src/pages/AuthPage.vue
+++ src/pages/AuthPage.vue
@@ -1,7 +1,70 @@
 <template>
-  <h1>Auth Page</h1>
+  <div class="segmented-control">
+    <input
+      type="radio"
+      id="login"
+      name="segment"
+      value="login"
+      v-model="chosenOption"
+      checked
+    />
+    <label for="login">Login</label>
+
+    <input
+      type="radio"
+      id="create-account"
+      name="segment"
+      value="create-account"
+      v-model="chosenOption"
+    />
+    <label for="create-account">Create Account</label>
+  </div>
+  <div v-if="chosenOption === 'login'">
+    <user-login></user-login>
+  </div>
+  <div v-else>
+    <create-account></create-account>
+  </div>
 </template>
 
 <script>
-export default {};
+import CreateAccount from "@/components/CreateAccount.vue";
+import UserLogin from "@/components/UserLogin.vue";
+
+export default {
+  components: {
+    CreateAccount,
+    UserLogin,
+  },
+  data() {
+    return {
+      chosenOption: "login",
+    };
+  },
+};
 </script>
+
+<style scoped>
+.segmented-control {
+  display: flex;
+  justify-content: space-between;
+  width: 300px;
+  margin: 20px auto;
+}
+
+input[type="radio"] {
+  display: none;
+}
+
+label {
+  flex: 1;
+  text-align: center;
+  padding: 10px;
+  cursor: pointer;
+  border: 1px solid #ccc;
+}
+
+input[type="radio"]:checked + label {
+  background-color: #e0e0e0;
+}
+</style>
