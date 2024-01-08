fbfb71d0bb373603678da8f04a73149a6b30d3d9
Osama <ossamaghoneim4@gmail.com>
2024-01-08 15:34:21 +0100

    STYLE: remove unused code

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -11,28 +11,6 @@ export default {
     signInWithEmailAndPassword(auth, data.email, data.password).then(() => {
       context.commit("setIsLoggedIn", true);
     });
-    /* axios
-      .post(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAT4a9hGBaASIC6wlCj3W1RKToSkeVD-PI",
-        {
-          email: data.email,
-          password: data.password,
-          retourSecureToken: true,
-        }
-      )
-      .then((res) => {
-        if (res.status === 200) {
-          context.commit("setLoginInfos", {
-            idToken: res.data.idToken,
-            email: res.data.email,
-            refreshToken: res.data.refreshToken,
-            expiresIn: res.data.expiresIn,
-            localId: res.data.localId,
-            registered: res.data.registered,
-          });
-          context.commit("setIsLoggedIn", true);
-        }
-      }); */
   },
   async createAccount(context, data) {
     createUserWithEmailAndPassword(auth, data.email, data.password).then(
@@ -49,14 +27,6 @@ export default {
     );
   },
   logOutUser(context) {
-    /* context.commit("setLoginInfos", {
-      idToken: null,
-      email: null,
-      refreshToken: null,
-      expiresIn: null,
-      localId: null,
-      registered: null,
-    }); */
     signOut(auth).then(() => {
       context.commit("setIsLoggedIn", false);
     });
