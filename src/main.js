4f38a14c49889603c54fbf452832ec0bf43e6104
Osama <ossamaghoneim4@gmail.com>
2024-01-08 15:58:27 +0100

    FEAT: install plugin for toasters

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import router from "./router";
 import "./main.css";
 import store from "./store/index";
 import "./firebase";
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
+app.use(Toast);
 
 app.mount("#app");
