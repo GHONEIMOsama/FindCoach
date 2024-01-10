c25e7bbccaad8346ff525657f7dfaf1856a0a5f6
Osama <ossamaghoneim4@gmail.com>
2024-01-10 11:06:13 +0100

    FEAT: make auth page unaccessible after login

--- src/router.js
+++ src/router.js
@@ -2,14 +2,26 @@ import { createRouter, createWebHistory } from "vue-router";
 import AuthPage from "./pages/AuthPage.vue";
 import CoachesList from "./pages/CoachesList.vue";
 import ContactCoach from "./pages/ContactCoach";
+import { auth } from "./firebase";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", redirect: "/coaches" },
-    { path: "/auth", component: AuthPage },
+    {
+      path: "/auth",
+      component: AuthPage,
+      beforeEnter: (to, from, next) => {
+        console.log(auth.currentUser);
+        if (auth.currentUser === null) {
+          next();
+        } else {
+          next("/coaches");
+        }
+      },
+    },
     { path: "/coaches", component: CoachesList },
-    { path: "/contact", component: ContactCoach },
+    { path: "/contact/:coachId", component: ContactCoach, props: true },
   ],
 });
 
