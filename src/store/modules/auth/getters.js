741c71f853203bd5643eb30c84b39ab710102a22
Osama <ossamaghoneim4@gmail.com>
2024-01-11 13:55:31 +0100

    DEL: remove unused method

--- src/store/modules/auth/getters.js
+++ src/store/modules/auth/getters.js
@@ -1,11 +1,5 @@
-import { auth } from "@/firebase";
-
 export default {
   getIsLoggedIn(state) {
     return state.isLoggedIn;
-  },
-  async getCurrentUser() {
-    await auth.authStateReady();
-    return auth.currentUser;
   },
 };
