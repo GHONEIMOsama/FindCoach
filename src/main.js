dceca580c83c9081185cea441a3c7ad46fd85b39
Osama <ossamaghoneim4@gmail.com>
2024-01-23 16:11:14 +0100

    FEAT: change position toast to bottom right

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import "./main.css";
 import store from "./store/index";
 import "./firebase";
-import Toast from "vue-toastification";
+import Toast, { POSITION } from "vue-toastification";
 import "vue-toastification/dist/index.css";
 /* import the fontawesome core */
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -28,7 +28,7 @@ const app = createApp(App);
 
 app.use(router);
 app.use(store);
-app.use(Toast);
+app.use(Toast, { position: POSITION.BOTTOM_RIGHT });
 app.component("fa-icon", FontAwesomeIcon);
 
 app.mount("#app");
