ee630626e9110cede445d702945a3af1561a72f1
Osama <ossamaghoneim4@gmail.com>
2024-01-23 10:38:37 +0100

    FEAT: change messages list design

--- src/pages/MessagesList.vue
+++ src/pages/MessagesList.vue
@@ -1,15 +1,31 @@
 <template>
-  <h1>Messages</h1>
-  <div v-for="msg in messages" :key="msg.id">
-    {{ msg.data.content }}
+  <div class="d-flex flex-row justify-content-center">
+    <h1 class="m-2">Messages</h1>
+  </div>
+  <div class="d-flex flex-row">
+    <div v-for="msg in messages" :key="msg.id" class="m-2">
+      <base-card>
+        <h5 class="card-title">
+          <span v-if="$store.getters['auth/getLoginInfos'].isCoach">{{
+            msg.data.from
+          }}</span>
+          <span v-else>{{ msg.data.to }}</span>
+        </h5>
+        <p class="card-text">
+          {{ msg.data.content }}
+        </p>
+      </base-card>
+    </div>
   </div>
 </template>
 
 <script>
 import { auth } from "@/firebase";
 import { mapActions } from "vuex";
+import BaseCard from "@/components/ui/BaseCard.vue";
 
 export default {
+  components: { BaseCard },
   data() {
     return {
       messages: [],
